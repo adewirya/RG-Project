1fbfd45e153cf50113b1d83c0ad7dd52904d667a
adewirya <adewirya6@gmail.com>
2020-12-26 01:59:26 +0700

    pertama

---
+++ app/Data.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Data extends Model
+{
+    protected $fillable = ['GroupName',
+    
+    'LeaderName', 
+     'Member1', 'Member2', 'Member3'];
+}
+
